6cd105602fcbb0172ea863e539f1f68b37c751b9
blankxx420 <briceguichou.dev@gmail.com>
2020-07-01 10:06:41 +0200

    api bug correction of data recuperation

--- Api.py
+++ Api.py
@@ -1,40 +1,38 @@
 import requests
 
 
-
 class Api:
 
     def __init__(self):
-        self.product = []
         self.paylod = {'action': 'process',
                        'json': 1,
                        'countries': 'France',
-                       'sort_by':'unique_scans_n',
+                       'sort_by': 'unique_scans_n',
                        'page_size': 1000,
                        'page': 1
                        }
 
     def get_product(self):
+        products = []
         r = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=self.paylod)
         result = r.json()
-        self.product.append(result['products'])
-        print(result)
-
+        products.append(result['products'])
+        return products
 
     def filter_data(self):
+        products = self.get_product()
         product_details = []
-        for product in self.product:
+        for product in products:
             for attribute in product:
-                attributes = { 'product_name': attribute['product_name_fr'],
-                               'nutriscore': attribute['nutrition_grades_tags'][0],
-                               'link': 'https://world.openfoodfacts.org/product/{}'.
-                                   format(attribute['code']),
-                               'id': attribute['code'],
-                               'store': attribute['stores'],
-                               'details': self.filter_generic_name(attribute),
-                               'category': self.filter_category(attribute)
-                            }
+                attributes = {'product_name': attribute['product_name_fr'],
+                              'nutriscore': attribute['nutrition_grades_tags'][0],
+                              'link': 'https://world.openfoodfacts.org/product/{}'.format(attribute['code']),
+                              'id': attribute['code'],
+                              'details': self.filter_generic_name(attribute),
+                              'store:': attribute['stores']
+                              }
                 product_details.append(attributes)
+        print(product_details)
 
     def filter_generic_name(self, attribute):
         try:
@@ -43,9 +41,6 @@ class Api:
             return 'Aucun'
 
 
-    def filter_category(self, attribute):
-        category_name = []
-        try:
-            return attribute['categories'].split(',')
-        except KeyError:
-            return 'Aucun'
+
+d = Api()
+d.filter_data()
